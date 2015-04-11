94fd32bdb4f341441309c672b9192124b6d1ad25
mlliou112 <myl10@duke.edu>
2015-04-11 05:25:43 -0400

    make script directory, commented crimedata script

--- scripts/crimedata.py
+++ scripts/crimedata.py
@@ -4,20 +4,26 @@ import os, sys
 import xml.etree.ElementTree as etree
 
 
-
 def main():
-    obj = etree.parse(raw_data_dir + os.path.sep +"crime_incidents_2013_plain.xml")
+
+    # Make DC XML crime data into iterable tree
+    obj = etree.parse(raw_data_dir + os.path.sep + "crime_incidents_2013_plain.xml")
     root = obj.getroot()
+
+    # File to store addresses
     f = open(derived_data_dir + os.path.sep + "crime_addresses.txt", 'w')
+
+    # Write each address to file
     for child in root:
         f.write(child[6].text + "\n")
     return
 
 
-
-
 if __name__ == "__main__":
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    # Variables for accessing different files in different directories
+    parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     raw_data_dir = os.path.join(parent_dir, "raw_data")
     derived_data_dir = os.path.join(parent_dir, "derived_data")
+
+    # Call main
     main()
